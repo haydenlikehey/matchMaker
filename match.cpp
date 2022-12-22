4373408f1619b1d7d2ccb17fa59f3dbc7aa851c3
Heyden <hgrace0001@gmail.com>
2022-12-21 20:42:51 -0600

    project init

---
+++ match.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "match.hpp"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+
+
+
+}
---
+++ match.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Profile {
+
+    string name;
+    int age;
+    string city;
+    string country;
+    string pronouns;
+    vector<string> hobbies;
+
+ public:
+
+
+
+
+};
